1430848b49b15a5fec02d4f5be8f216ff4b59dcd
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-06-03 23:28:03 +0500

    Home Page plus Logout Functionality done..

--- app/src/main/java/com/example/hc/HomeActivity.java
+++ app/src/main/java/com/example/hc/HomeActivity.java
@@ -1,9 +1,15 @@
 package com.example.hc;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +26,22 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SharedPreferences sharedpreferences = getSharedPreferences ("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedpreferences.getString("username",  "").toString();
+        Toast.makeText(getApplicationContext(),  "Welcome "+username, Toast.LENGTH_SHORT).show();
+        CardView exit = findViewById(R.id.cardExit);
+
+exit.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        SharedPreferences. Editor editor = sharedpreferences.edit();
+        editor.clear();
+        editor.apply();
+        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+    }
+});
+
+
     }
 }
