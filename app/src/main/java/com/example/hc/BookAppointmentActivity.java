0102171d9e5e6fb3aa234cdeb8119db4a0cfd61c
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-06-08 22:52:49 +0500

    Book Appointment Activity, Date/Time added..

--- app/src/main/java/com/example/hc/BookAppointmentActivity.java
+++ app/src/main/java/com/example/hc/BookAppointmentActivity.java
@@ -1,9 +1,16 @@
 package com.example.hc;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +18,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class BookAppointmentActivity extends AppCompatActivity {
 
     EditText ed1,ed2,ed3,ed4;
     TextView tv;
+
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
+
+    private Button dateButton,timeButton,btnBook,btnBack;
 
 
     @Override
@@ -35,6 +49,12 @@ public class BookAppointmentActivity extends AppCompatActivity {
         ed2 = findViewById(R.id.editTextAppAddress);
         ed3 = findViewById(R.id.editTextAppContactNum);
         ed4 = findViewById(R.id.editTextFees);
+        btnBook = findViewById(R.id.buttonBookAppointment);
+        btnBack = findViewById(R.id.buttonAppBack);
+
+        dateButton = findViewById(R.id.buttonAppDate);
+        timeButton = findViewById(R.id.buttonAppTime);
+
 
         // Edit Texts Not Editable
         ed1.setKeyListener(null);
@@ -57,5 +77,69 @@ public class BookAppointmentActivity extends AppCompatActivity {
         ed3.setText(contact);
         ed4.setText("Fees : " + fees + " /-");
 
+        // DAte Picker
+        initDatePicker();
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+        btnBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // -------------------------------------------Editable
+
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BookAppointmentActivity.this,FindDoctorActivity.class));
+            }
+        });
+
+
     }
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                i1 = i1 + 1;
+                dateButton.setText(i2 + "/" + i1 + "/" + i);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+        datePickerDialog = new DatePickerDialog( this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMinDate(cal.getTimeInMillis()+86400000);
+    }
+
+    private void initTimePicker(){
+
+    TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+    @Override
+    public void onTimeSet (TimePicker timePicker, int i, int i1) {
+        timeButton.setText(i+" : "+i1);
+
+    }
+    };
+    Calendar cal = Calendar.getInstance();
+    int hrs = cal.get(Calendar.HOUR);
+    int mins = cal.get(Calendar.MINUTE);
+    int style = AlertDialog.THEME_HOLO_DARK;
+    timePickerDialog = new TimePickerDialog(this, style, timeSetListener, hrs, mins,  true);
+
+        }
+
 }
